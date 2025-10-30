84145049dbfc699e9ce00dd3bfc2eae3cc170cd5
charan-happy <nagacharan4286@gmail.com>
2025-10-30 10:16:44 +0000

    updated projects

---
+++ Containerization-Expert/vue-app/vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Hello from Vue.js 🚀</h1>
+    <p>This app runs inside a Docker container on EC2.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 100px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
+
